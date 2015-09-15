95d18c0fb87a0ad2fe9b7f24df010c14e498839f
Valery Barysok <valery.barysok@gmail.com>
2015-09-15 03:59:45 +0300

    adapts JSON-js/cycle.js to node. adds tests.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # json-cycle
-Utilities provide ability to encode/decode cyclical structures.
+
+[![Join the chat at https://gitter.im/valery-barysok/sails-hook-dev-spirit][gitter-join-chat-image]][gitter-channel-url]
+[![NPM Version][npm-version-image]][npm-url]
+[![NPM Downloads][npm-downloads-image]][npm-url]
+[![Node.js Version][node-image]][node-url]
+
+[![Build Status][travis-image]][travis-url]
+[![Dependency Status][dependencies-image]][dependencies-url]
+[![devDependency Status][dev-dependencies-image]][dev-dependencies-url]
+[![Coverage Status][coveralls-image]][coveralls-url]
+
+[![NPM][npm-image]][npm-url]
+
+Utilities provide ability to encode/decode circular structures for converting to and from JSON.
+
+Based on [JSON-js](https://github.com/douglascrockford/JSON-js)
+
+## Install
+
+In your Sails project:
+
+```
+npm install json-cycle --save
+```
+
+## License
+MIT &copy; 2015 Valery Barysok, Douglas Crockford
+
+[npm-version-image]: https://img.shields.io/npm/v/json-cycle.svg?style=flat-square
+[npm-downloads-image]: https://img.shields.io/npm/dm/json-cycle.svg?style=flat-square
+[npm-image]: https://nodei.co/npm/json-cycle.png?downloads=true&downloadRank=true&stars=true
+[npm-url]: https://npmjs.org/package/json-cycle
+[travis-image]: https://img.shields.io/travis/valery-barysok/json-cycle/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/valery-barysok/json-cycle
+[dependencies-image]: https://david-dm.org/valery-barysok/json-cycle.svg?style=flat-square
+[dependencies-url]: https://david-dm.org/valery-barysok/json-cycle
+[dev-dependencies-image]: https://david-dm.org/valery-barysok/json-cycle/dev-status.svg?style=flat-square
+[dev-dependencies-url]: https://david-dm.org/valery-barysok/json-cycle#info=devDependencies
+[coveralls-image]: https://img.shields.io/coveralls/valery-barysok/json-cycle/master.svg?style=flat-square
+[coveralls-url]: https://coveralls.io/r/valery-barysok/json-cycle?branch=master
+[node-image]: https://img.shields.io/node/v/json-cycle.svg?style=flat-square
+[node-url]: http://nodejs.org/download/
+[gitter-join-chat-image]: https://badges.gitter.im/Join%20Chat.svg?style=flat-square
+[gitter-channel-url]: https://gitter.im/valery-barysok/json-cycle
---
+++ test/circular.test.js
@@ -0,0 +1,177 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var cycle = require('../');
+var decycle = cycle.decycle;
+var retrocycle = cycle.retrocycle;
+
+describe('Json Cycle', function () {
+
+  var arr = [];
+  var prev = null;
+  var cnt = 100;
+  for (var i = 0; i < cnt; ++i) {
+    prev = arr[i] = {
+      prev: prev
+    };
+  }
+  arr[0].prev = arr[cnt - 1];
+
+  it('should failed with TypeError: Converting circular structure to JSON', function (done) {
+
+    expect(function () {
+      JSON.stringify(arr);
+    }).to.throw(Error);
+
+    done();
+  });
+
+  describe("#decycle + #JSON.stringify", function () {
+
+    it('should not failed after decycle', function (done) {
+
+      expect(function () {
+        JSON.stringify(decycle(arr));
+      }).to.not.throw(Error);
+
+      done();
+    });
+
+    it("should not failed with circular objects", function () {
+      var foo = {
+        prop: "dummy"
+      };
+      foo.bar = foo;
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular objects with intermediaries", function () {
+      var foo = {
+        prop: "dummy"
+      };
+      foo.bar = {
+        foo: foo
+      };
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular objects deeper", function () {
+      var foo = {
+        prop: "dummy",
+
+        bar: {
+          prop: "dummy"
+        }
+      };
+      foo.bar.self = foo.bar;
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular objects deeper with intermediaries", function () {
+      var foo = {
+        prop: "dummy",
+
+        bar: {
+          prop: "dummy"
+        }
+      };
+      foo.bar.zoo = {
+        self: foo.bar
+      };
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular objects in an array", function() {
+      var foo = {
+        prop: "dummy"
+      };
+      foo.bar = [foo, foo];
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular objects deeper in an array", function() {
+      var foo = {
+        prop: "dummy",
+
+        bar: [{
+          prop: "dummy"
+        }, {
+          prop: "dummy2"
+        }]
+      };
+      foo.bar[0].self = foo.bar[0];
+      foo.bar[1].self = foo.bar[1];
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular arrays", function() {
+      var foo = [];
+      foo.push(foo);
+      foo.push(foo);
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular arrays with intermediaries", function() {
+      var foo = [];
+      foo.push({ bar: foo });
+      foo.push({ bar: foo });
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular repeated objects in objects", function() {
+      var foo = {};
+      var bar = {
+        prop: "dummy"
+      };
+      foo.bar1 = bar;
+      foo.bar2 = bar;
+
+      expect(function () {
+        JSON.stringify(decycle(foo));
+      }).to.not.throw(Error);
+    });
+
+    it("should not failed with circular repeated objects in arrays", function() {
+      var foo = {
+        prop: "dummy"
+      };
+      var bar = [foo, foo];
+
+      expect(function () {
+        JSON.stringify(decycle(bar));
+      }).to.not.throw(Error);
+    });
+  });
+
+  it('should be the the same after retrocycling on decycled structure', function (done) {
+
+    expect(arr).to.deep.equal(retrocycle(decycle(arr)));
+
+    done();
+  });
+});
